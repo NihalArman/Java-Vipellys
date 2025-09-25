0edd4eb86cc269e02115d1050e8761dfe31fe747
Arman Nihal <armansaliknihal@gmail.com>
2025-09-25 20:06:48 +0300

    Fix bug: Background color was not visible to the screen at launch. Calling setVisible method for the frame very end fixes the bug

--- Java Vipellys/src/Main.java
+++ Java Vipellys/src/Main.java
@@ -6,11 +6,12 @@ public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
         frame.setSize(500, 500);
-        frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel panel = getPanel();
         frame.add(panel);
+
+        frame.setVisible(true);
     }
 
     private static JPanel getPanel() {
