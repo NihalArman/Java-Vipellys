225a7cac54c6e3d3e4aa11d10a7296c1a5258846
Arman Nihal <armansaliknihal@gmail.com>
2025-09-26 14:46:13 +0300

    Remove hardcoded items. Create getter and setter method for the items.
    Also make functions non static

--- Java Vipellys/src/Main.java
+++ Java Vipellys/src/Main.java
@@ -2,7 +2,9 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Main {
-    public static void main(String[] args) {
+    private DefaultListModel<String> items = new DefaultListModel<>();
+
+    public void main(String[] args) {
         JFrame frame = new JFrame();
         frame.setSize(500, 500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -18,52 +20,54 @@ public class Main {
         frame.setVisible(true);
     }
 
-    private static JPanel initialPanel() {
+    private JPanel initialPanel() {
         JPanel panel = new JPanel();
         panel.setLayout(null);
         panel.setBackground(Color.GRAY);
         return panel;
     }
 
-    private static JLabel titleLabel() {
+    private JLabel titleLabel() {
         JLabel label = new JLabel();
         label.setBounds(100, 0, 300, 20);
         label.setText("The best application in the world");
         return label;
     }
 
-    private static JTextField userInputTextField() {
+    private JTextField userInputTextField() {
         JTextField textField = new JTextField();
         textField.setColumns(20);
         textField.setBounds(100, 50, 200, 30);
         return textField;
     }
 
-    private static JButton saveButton() {
+    private JButton saveButton() {
         JButton button = new JButton();
         button.setText("save");
         button.setBounds(140, 100, 120, 30);
         return button;
     }
 
-    private static JButton showButton() {
+    private JButton showButton() {
         JButton button = new JButton();
         button.setText("show items");
         button.setBounds(170, 420, 120, 30);
         return button;
     }
 
-    private static JScrollPane displayItems() {
+    private JScrollPane displayItems() {
         JScrollPane scrollPane = new JScrollPane(getItems());
         scrollPane.setBounds(60, 200, 400, 200);
         return scrollPane;
     }
 
-    private static JList<String> getItems() {
-        DefaultListModel<String> items = new DefaultListModel<>();
-        items.addElement("Item1");
-        items.addElement("Item2");
-        items.addElement("Item3");
-        return new JList<>(items);
+    private JList<String> getItems() {
+        return new JList<>(this.items);
+    }
+
+    private void saveItems(String text) {
+        if (!text.isEmpty()) {
+            this.items.addElement(text);
+        }
     }
 }
